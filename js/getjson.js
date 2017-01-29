2e5c864673fe0818683963acf5e760290cdc35a6
Andrea McIntosh <akmcinto@ualberta.ca>
2017-01-28 18:18:19 -0700

    Ugly alerts

--- js/getjson.js
+++ js/getjson.js
@@ -1,16 +1,18 @@
-var invocation = new XMLHttpRequest();
-var url = 'http://services.swpc.noaa.gov/products/solar-wind/plasma-5-minute.json';
+var plasmaurl = 'http://services.swpc.noaa.gov/products/solar-wind/plasma-5-minute.json';
+var alertsurl = 'http://services.swpc.noaa.gov/products/alerts.json';
 
-// $('#index').on('pageShow', function(e) {
-//     if(invocation) {
-//         invocation.open('GET', url, true);
-//         invocation.onreadystatechange = handler;
-//         invocation.send();
-//     }
-// }
+$.getJSON(alertsurl, function(data) {
+    $.each(data, function(index, element) {
+        // var datas = element.toString().split(',');
+        $('#alerts').append($('<p>', {
+                text: JSON.stringify(element)
+            }
+        ))
+    });
+});
 
 // http://stackoverflow.com/questions/8951810/how-to-parse-json-data-with-jquery-javascript
-$.getJSON(url, function(data) {
+$.getJSON(plasmaurl, function(data) {
     $.each(data, function(index, element) {
         if (index == 2) {
             var datas = element.toString().split(',');
